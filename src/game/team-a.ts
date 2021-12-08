547d0ab729085dbf9782b7f15dd350436368406e
AlexandrKhoroshikh <xad71rus@gmail.com>
2021-12-08 16:20:56 +0300

    feat: add better contra-camper strategy

--- src/game/team-a.ts
+++ src/game/team-a.ts
@@ -1,38 +1,52 @@
 import type { GameState, Action, Bot, Position, Dir } from "./model/types";
 
 const teamA = (game: GameState): Action => {
-  game.stash.last =
-    typeof game.stash.last !== "undefined" ? game.stash.last : 0;
-  const next = game.stash.last + 1;
-  game.stash.last = next < game.myBots.length ? next : 0;
-  const current = game.myBots[game.stash.last];
+  // rules are the same as for react hooks, order must not change
+  const [last, setLast] = game.useStash(0);
+  const next = (last + 1) < game.myBots.length ? last + 1 : 0;
+  setLast(next)
+  const current = game.myBots[last];
+
+
+  for (let i = 0; i < game.myBots.length; i++) {
+    const my = game.myBots[i];
+    const target = game.lib.findClosest(my, game.enemyBots);
+    const dir = game.lib.getDir(my, target);
+    if (my.viewDir === dir && game.lib.inGunRange(my, target)) {
+      return {
+        type: "shoot",
+        id: my.id
+      }
+    }
+
+    const d = game.lib.getDistance(my, target);
+    if (d < 3) {
+      return {
+        type: "move",
+        id: my.id,
+        dir,
+      }
+    }
+  }
 
   const closest = game.lib.findClosest(current, game.enemyBots);
   const closestDir = game.lib.getDir(current, closest);
-
-  if (closestDir === current.viewDir && game.lib.inGunRange(current, closest)) {
-    return {
-      type: "shoot",
-      id: current.id,
-    };
-  }
 
   // defence move example
   if (game.lib.inGunRange(closest, current)) {
     return {
       type: "move",
-      dir: game.lib.getRot(closestDir, 1),
+      dir: game.lib.getRot(closestDir, -1),
       id: current.id,
     };
   }
 
   const distance = game.lib.getDistance(current, closest);
   const far = distance > 4;
-  const canHit = distance < 3
 
   return {
     type: "move",
-    dir: (canHit || far) ? closestDir : game.lib.getRot(closestDir, 1),
+    dir: (far) ? closestDir : game.lib.getRot(closestDir, 1),
     id: current.id,
   };
 };
